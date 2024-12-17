cd77b804a363829a86f12894759b851febdb32f8
kike454 <enriquecomu5@gmail.com>
2024-12-17 18:11:05 +0100

    socket implementadosgit status

---
+++ socket/express_test/public/javascripts/script.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+        var form = document.getElementById('formChat');
+        var input = document.getElementById('inputChat');
+        var messages = document.getElementById('messages');
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (input.value) {
+                socket.emit('chat message', input.value);
+                input.value = '';
+            }
+        });
+
+        socket.on('chat message', function(msg) {
+            var item = document.createElement('li');
+            item.textContent = msg;
+            messages.appendChild(item);
+        });
